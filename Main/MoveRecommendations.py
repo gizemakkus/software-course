09c49a3f069cef859d769bcb4f08ce1b766fbbe2
Gizem Akkuş <gizemakkus@Gizems-MacBook.local>
2014-10-16 17:40:56 +0300

    initial commit

---
+++ Main/MoveRecommendations.py
@@ -0,0 +1,8 @@
+__author__ = 'gizemakkus'
+
+def fnc():
+    a=raw_input("enter string")
+    print len(a)
+
+#from vcs commit changes chech-in codels to the onlıne repository Gİthub, Subversion, cvs
+# update project check out codels to your machine , local repository

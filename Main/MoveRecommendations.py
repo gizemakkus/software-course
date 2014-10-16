db6f3b5a857a936caae49ec80b638abd99094a5a
gizemakkus <gizemakkus@std.sehir.edu.tr>
2014-10-16 17:58:51 +0300

    print hello

--- Main/MoveRecommendations.py
+++ Main/MoveRecommendations.py
@@ -3,6 +3,7 @@ __author__ = 'gizemakkus'
 def fnc():
     a=raw_input("enter string")
     print len(a)
+    print 'lol'
 
 #from vcs commit changes chech-in codels to the onlıne repository Gİthub, Subversion, cvs
 # update project check out codels to your machine , local repository
